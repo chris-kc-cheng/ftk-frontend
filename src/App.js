cbf8101465f6da070cad2ba3763ea7656cb83114
chris-kc-cheng <kc1116@gmail.com>
2023-11-28 16:16:07 -0500

    Fetch API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const getContents = () => {
+    fetch("http://127.0.0.1:5000/notes/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      })
+  }
+  useEffect(() => {
+    getContents()
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button type="button" onClick={() => {
+        fetch("http://127.0.0.1:5000/notes/new", {
+          method: "POST",
+          headers: {            
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({content: "Some text 😋"})
+        })
+      }}>Click to add
+      </button>
+      {data.map(note =>
+        <div>
+          <Markdown remarkPlugins={[remarkGfm]}>{note.content}</Markdown>
+        </div>
+      )}
+    </>
   );
 }
 
