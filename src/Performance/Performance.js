43e815d133e3dee9becc97e531dcf7c502b503f0
chris-kc-cheng <kc1116@gmail.com>
2023-12-04 15:14:03 -0500

    Added MUI Charts

--- src/Performance/Performance.js
+++ src/Performance/Performance.js
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { authFetch } from "../Auth";
 import Stack from '@mui/material/Stack';
 import { BarChart } from '@mui/x-charts/BarChart';
 import { LineChart } from '@mui/x-charts/LineChart';
@@ -7,58 +8,70 @@ import { ScatterChart } from '@mui/x-charts/ScatterChart';
 
 const Performance = () => {
 
-    useEffect(() => {
+    const [data, setData] = useState();
 
+    useEffect(() => {
+        authFetch(`${process.env.REACT_APP_API_ROOT}/performance/test`)
+        .then((response) => response.json())
+        .then((data) => {
+            setData(data);
+        })
     }, []);
 
     return (
         <Stack>
             <h1>Performance</h1>
 
-            <LineChart
-                xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-                series={[
-                    {
-                        curve: "linear", label: 'Fund',
-                        data: [2, 5.5, 2, 8.5, 1.5, 5],
-                    },
-                    {
-                        curve: "linear", label: 'Benchmark',
-                        data: [2, 3, 4, 7, 1, 3],
+            {data && data.lineChartData &&
+                <LineChart
+                    xAxis={[{ scaleType: 'point', data: data.lineChartData.index }]}
+                    series={
+                        data.lineChartData.columns.map((column, i) => {
+                            return {                                
+                                label: column,
+                                data: data.lineChartData.data.map(row => row[i]),
+                                curve: "linear"
+                            }
+                        })
                     }
-                ]}
-                width={500}
-                height={300}
-            />
+                    width={500}
+                    height={300}
+                />
+            }
 
-            <BarChart
-                xAxis={[{ scaleType: 'band', data: [2021, 2022, 2023] }]}
-                series={[{
-                    label: "US/Canada",
-                    data: [4, 3, 5],
-                    stack: 'stack1'
-                }, { label: "Europe", data: [1, 6, 3], stack: 'stack1' }, { label: "Asia", data: [2, 5, 4], stack: 'stack1' }]}
-                width={500}
-                height={300}
-            />
+            {data && data.barChartData && 
+                <BarChart
+                    xAxis={[{ scaleType: 'band', data: data.barChartData.index }]}
+                    series={data.barChartData.columns.map((column, i) => {
+                        return {
+                            label: column,
+                            data: data.barChartData.data.map(row => row[i]),
+                            stack: 'byMonth'    
+                        }
+                    })}
+                    width={500}
+                    height={300}
+                />
+            }
 
-            <PieChart
-                series={[
-                    {
-                        data: [
-                            { value: 55, label: 'Equity' },
-                            { value: 40, label: 'Fixed Income' },
-                            { value: 5, label: 'Cash' },
-                        ],
+            {data && data.pieChartData &&
+                <PieChart
+                    series={[{
+                        data: data.pieChartData.columns.map((column, i) => {                            
+                            return {
+                                label: column,
+                                value: data.pieChartData.data[i]
+                            }
+                        }),
                         innerRadius: 30,
                         outerRadius: 100,
                         paddingAngle: 5,
                         cornerRadius: 5,
-                    },
-                ]}
-                width={500}
-                height={300}
-            />
+                    }]}
+                    width={500}
+                    height={300}
+                />
+            }
 
             <ScatterChart
                 width={500}
